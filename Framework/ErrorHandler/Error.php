8d20c2beba6a5d96887a60206fdf044dd3e9ff58
Julesse0 <jules.hospital@etu.univ-amu.fr>
2023-09-29 11:27:26 +0200

    creation des getters et du constructeur

--- Framework/ErrorHandler/Error.php
+++ Framework/ErrorHandler/Error.php
@@ -2,7 +2,57 @@
 
 namespace ErrorHandler;
 
-class Error
+class Error implements Throwable
 {
+    private $message;
+    private $code;
+    private $file;
+    private $line;
+    private $niveau;
+    private $description;
 
+    public function __construct($message = "", $code = 0)
+    {
+
+    }
+
+    private function getMessage()
+    {
+    }
+
+
+    private function getCode()
+    {
+
+    }
+    private function getFile()
+    {
+
+    }
+    private function getLine()
+    {
+
+    }
+    private function getTraceAsString()
+    {
+
+    }
+    private function toString()
+    {
+
+    }
+    private function clone()
+    {
+
+    }
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function getNiveau(): int
+    {
+        return $this->niveau;
+
+    }
 }
