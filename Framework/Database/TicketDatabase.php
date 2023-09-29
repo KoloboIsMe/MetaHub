8c1209dd77df3c61ee96c695a25e2c933a672064
Myke Lieto <myke.lieto@etu.univ-amu.fr>
2023-09-29 10:57:30 +0200

    Ajout fonction Insert et constructeur pour Ticket

--- Framework/Database/TicketDatabase.php
+++ Framework/Database/TicketDatabase.php
@@ -7,4 +7,11 @@ class TicketDatabase implements Database
     public function getData(){
         return $this->data;
     }
+
+    public function insertTicket($ticket){
+
+        $query = "INSERT INTO TICKET ($message, $author) VALUES $ticket ";
+        $id = "SELECT ID FROM TICKET";
+        $ticket->setId($id);
+    }
 }
--- Framework/Entity/Ticket.php
+++ Framework/Entity/Ticket.php
@@ -10,4 +10,16 @@ class Ticket implements Entity
     private $date;
     private $author;
     private $category;
+
+    public function __construct($title = NULL, $message = NULL, $date = NULL, $author = NULL, $category = NULL){
+        $this->title = $title;
+        $this->message = $message;
+        $this->date = $date;
+        $this->author = $author;
+        $this->category = $category;
+    }
+
+    public function setId($newID){
+        $this->ID = $newID;
+    }
 }
