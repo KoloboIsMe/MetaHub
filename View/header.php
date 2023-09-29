bcb2103baeef6c875d7e62b2f628186c1a35f34c
sookdp <sokhna.diop.1@etu.univ-amu.fr>
2023-09-29 09:41:32 +0200

    Création de la barre de navigation

--- View/header.php
+++ View/header.php
@@ -10,7 +10,23 @@
         <link href="_assets/styles/style.css" rel="stylesheet" type="text/css" />
     </head>
     <body>
-
+        <header>
+            <input type="checkbox" id="nav_check" hidden>
+            <nav>
+                <ul>
+                    <li class="active"><a <img src="/_assets/images/home.png"/> href="index.html" class="active"></a></li>
+                    <li><a href="">Posts</a></li>
+                    <li><a href="">Créer un Post</a></li>
+                    <li><a href="">Catégories</a></li>
+                    <li><a <img src="/_assets/images/login.png"> href="" </a></li>
+                </ul>
+            </nav>
+            <label for="nav_check" aria-label="toggle curtain navigation" class="hamburger">
+                <div class="line l1"></div>
+                <div class="line l2"></div>
+                <div class="line l3"></div>
+            </label>
+        </header>
 <?php
 }
 ?>
