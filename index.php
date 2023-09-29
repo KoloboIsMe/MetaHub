5bcab67b2a31de2e1dc00688a96579f6dae29b4f
MaximeTAMARINT22003230 <maxime.tamarin@etu.univ-amu.fr>
2023-09-29 09:27:19 +0200

    test fonctionnement page login

--- index.php
+++ index.php
@@ -1,13 +1,31 @@
-<!DOCTYPE html>
 <html>
 <head>
-    <title>Ma première page PHP</title>
+    <meta charset="utf-8">
+    <!-- importer le fichier de style -->
+    <link rel="stylesheet" href="./_assets/styles/style_test.css" media="screen" type="text/css" />
 </head>
 <body>
-<?php
-echo "Hello, World!";
-echo "<br>";
-echo "je teste si la connexion FTP fonctionne";
-?>
+<div id="container">
+    <!-- zone de connexion -->
+
+    <form action="verification.php" method="POST">
+        <h1>Connexion</h1>
+
+        <label><b>Nom d'utilisateur</b></label>
+        <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
+
+        <label><b>Mot de passe</b></label>
+        <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+
+        <input type="submit" id='submit' value='LOGIN' >
+        <?php
+        if(isset($_GET['erreur'])){
+            $err = $_GET['erreur'];
+            if($err==1 || $err==2)
+                echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+        }
+        ?>
+    </form>
+</div>
 </body>
 </html>
