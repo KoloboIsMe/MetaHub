dcc2d4e3ae51bab4c9ff09a63c0daf14044f6ba1
Julesse0 <jules.hospital@etu.univ-amu.fr>
2023-10-02 09:41:59 +0200

    implémentation des fonctions

---
+++ Framework/ErrorHandler/Exception.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace ErrorHandler;
+
+class Exception implements Throwable
+{
+    private $message;
+    private $code;
+    private $file;
+    private $line;
+    private $level;
+    private $description;
+    private $time;
+
+    public function __construct($message = "", $code = 0)
+    {
+
+    }
+
+    public function getMessage()
+    {
+        return $this->message;
+    }
+    public function getCode()
+    {
+        return $this->code;
+    }
+    public function getFile()
+    {
+        return $this->file;
+    }
+    public function getLine()
+    {
+        return $this->line;
+    }
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function getLevel()
+    {
+        return $this->niveau;
+
+    }
+
+    public function getTime()
+    {
+        $t=time() ;
+        echo(date("Y-m-d H:i:s",$t));
+        return $this->temps;
+    }
+}
