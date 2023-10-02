37ca1f8a788475181387ecac5c44b9059f432dc7
Julesse0 <jules.hospital@etu.univ-amu.fr>
2023-10-02 12:09:11 +0200

    création du tableau des Exceptions

--- Framework/MHErrorHandler/MHCatcher.php
+++ Framework/MHErrorHandler/MHCatcher.php
@@ -1,8 +1,40 @@
 <?php
 
-namespace MHErrorHandler;
+namespace MetaHubFramework\ErrorHandler {
 
-class MHCatcher
-{
+    class MHCatcher
+    {
+        private $tableau;
+        private $level;
 
+        public function __construct($tableau,$level){
+            $this->tableau = $tableau;
+            $this->level = $level;
+        }
+
+        public function getTableau()
+        {
+            return $this->tableau;
+        }
+
+        public function setTableau($tableau): void
+        {
+            $this->tableau = $tableau;
+        }
+
+        public function sort($tableau)
+        {
+
+        }
+
+        public function getLevel()
+        {
+            return $this->level;
+        }
+
+        public function setLevel($level): void
+        {
+            $this->level = $level;
+        }
+    }
 }
