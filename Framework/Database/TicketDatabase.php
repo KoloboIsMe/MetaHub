20ebc6593dfa65bef54d3a9acf8cf6d234a00957
Myke Lieto <myke.lieto@etu.univ-amu.fr>
2023-10-02 09:44:45 +0200

    Création des constantes pour les formats des tickets et création de la fonction pour vérifier les formats

--- Framework/Database/TicketDatabase.php
+++ Framework/Database/TicketDatabase.php
@@ -3,10 +3,59 @@
 class TicketDatabase implements Database
 {
     //déclarer des constantes pour les limites de tailles
+    private $ID_LENGTH = 10;
+    private $TITLE_LENGHT = 10;
+    private $MESSAGE_LENGTH = 280;
+    private $DATE_FORMAT =  'd/m/Y';
     private $data;
 
     public function getData(){
         return $this->data;
+    }
+    public function verifTicket($ticket){
+        //faire des if pour verif les tailles et mettre l'exception dedans
+        $ticketID = $ticket->getID();
+        $ticketTitle = $ticket->getTitle();
+        $ticketMessage = $ticket->getMessage();
+        $ticketDate = $ticket->getDate();
+        $ticketAuthor = $ticket->getAuthor();
+        $ticketCategory = $ticket->getCategory();
+
+        if(strlen($ticketID) == self::$ID_LENGTH){
+            //le ticket peut se créer
+        }
+        else
+            //le ticket ne peut pas se créer
+
+        if(strlen($ticketTitle) == self::$TITLE_LENGHT){
+            //le ticket peut se créer
+        }
+        else
+            //le ticket ne peut pas se créer
+
+        if(strlen($ticketMessage) == self::$MESSAGE_LENGTH){
+            //le ticket peut se créer
+        }
+        else
+            //le ticket ne peut pas se créer
+
+        if($ticketDate->format == self::$DATE_FORMAT){
+            //le ticket peut se créer
+        }
+//        else
+//            //le ticket ne peut pas se créer
+
+//        if(/*l'auteur se trouve dans la base de donnée*/){
+//            //le ticket peut se créer
+//        }
+//        else
+//            //le ticket ne peut pas se créer
+
+//        if(/*la catégorie se trouve dans la base de donnée*/){
+//            //le ticket peut se créer
+//        }
+//        else
+//            //le ticket ne peut pas se créer
     }
 
     public function insertTicket($ticket){
@@ -17,9 +66,10 @@ class TicketDatabase implements Database
         $ticketAuthor = $ticket->getAuthor();
         $ticketCategory = $ticket->getCategory();
 
+
         //verifier si les valeurs sont conforme
-        //faire des if pour verif les tailles et mettre l'exception dedans
-        // //
+        verifTicket($ticket);
+
 
         $query = "INSERT INTO TICKET ($ticketID, $ticketTitle, $ticketMessage, $ticketDate, $ticketAuthor, $ticketCategory) VALUES $ticket ";
         $id = "SELECT ID FROM TICKET";
