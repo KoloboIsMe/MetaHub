b65f6522b0c3b9843722f64fff57c9c5eef1f988
Julesse0 <jules.hospital@etu.univ-amu.fr>
2023-10-02 12:09:11 +0200

    création du tableau des Exceptions

--- Framework/MHErrorHandler/MHException.php
+++ Framework/MHErrorHandler/MHException.php
@@ -1,53 +1,56 @@
 <?php
 
-namespace MHErrorHandler;
+namespace MetaHubFramework\ErrorHandler {
 
-class MHException implements Throwable
-{
-    private $message;
-    private $code;
-    private $file;
-    private $line;
-    private $level;
-    private $description;
-    private $time;
+    class MHException
+    {
+        private $message;
+        private $code;
+        private $file;
+        private $line;
+        private $description;
+        private $time;
 
-    public function __construct($message = "", $code = 0)
-    {
+        public function __construct($message, $code, $file, $line, $description, $time)
+        {
+            $this->message = $message;
+            $this->code = $code;
+            $this->file = $file;
+            $this->line = $line;
+            $this->description = $description;
+            $this->time = $time;
+        }
 
-    }
+        public function getMessage()
+        {
+            return $this->message;
+        }
 
-    public function getMessage()
-    {
-        return $this->message;
-    }
-    public function getCode()
-    {
-        return $this->code;
-    }
-    public function getFile()
-    {
-        return $this->file;
-    }
-    public function getLine()
-    {
-        return $this->line;
-    }
-    public function getDescription()
-    {
-        return $this->description;
-    }
+        public function getCode()
+        {
+            return $this->code;
+        }
 
-    public function getLevel()
-    {
-        return $this->level;
+        public function getFile()
+        {
+            return $this->file;
+        }
 
-    }
+        public function getLine()
+        {
+            return $this->line;
+        }
 
-    public function getTime()
-    {
-        $t=time() ;
-        echo(date("Y-m-d H:i:s",$t));
-        return $this->time;
-    }
+        public function getDescription()
+        {
+            return $this->description;
+        }
+
+        public function getTime()
+        {
+            $t = time();
+            echo(date("Y-m-d H:i:s", $t));
+            return $this->time;
+        }
+}
 }
