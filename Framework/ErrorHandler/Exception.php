510f8b279a7d26dc04f6c0ff745d605b23bd5bea
Julesse0 <jules.hospital@etu.univ-amu.fr>
2023-10-02 09:53:50 +0200

    maj des exceptions

--- Framework/ErrorHandler/Exception.php
+++ Framework/ErrorHandler/Exception.php
@@ -40,7 +40,7 @@ class Exception implements Throwable
 
     public function getLevel()
     {
-        return $this->niveau;
+        return $this->level;
 
     }
 
@@ -48,6 +48,6 @@ class Exception implements Throwable
     {
         $t=time() ;
         echo(date("Y-m-d H:i:s",$t));
-        return $this->temps;
+        return $this->time;
     }
 }
