62e5b8900e47f47e3214d9dc9c783fd0b1ab2cab
Gabriel <gabriel.rouge@etu.univ-amu.fr>
2023-10-06 11:16:39 +0200

    Catcher->exception
    Signed-off-by: Gabriel <gabriel.rouge@etu.univ-amu.fr>

--- test.php
+++ test.php
@@ -1,6 +1,10 @@
 <?php
-use MetaHubFramework\ErrorHandler\MHException;
-include "Framework\MHErrorHandler\MHException.php";
-$t= new MHException(getTime());
+
+require_once "Framework/MHErrorHandler/Catcher.php";
+require_once "Framework/MHErrorHandler/ExceptionType.php";
+$catcher = new Catcher();
+//$catcher->exception(\MHErrorHandler\ExceptionType::Exception, "Exception test", 0);
+$catcher->exception();
+echo $catcher;
 ?>
 
