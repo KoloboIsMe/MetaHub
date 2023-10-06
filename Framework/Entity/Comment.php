79de95b991f20c7a92ba21f8209e40dc1407e6ed
Myke Lieto <myke.lieto@etu.univ-amu.fr>
2023-10-06 09:37:19 +0200

    Modification de la classe Comment. Ajout des attributs, constructeur et getter

--- Framework/Entity/Comment.php
+++ Framework/Entity/Comment.php
@@ -4,9 +4,56 @@ namespace Entity;
 
 class Comment
 {
-    private $ID;
+    private $comment_ID;
     private $text;
     private $date;
     private $author;
     private $ticket;
+
+    public function __construct($comment_ID, $text, $author, $ticket)
+    {
+        $this->comment_ID = $comment_ID;
+        $this->text = $text;
+        $this->date = date('d/m/Y');
+        $this->author = $author;
+        $this->ticket = $ticket;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCommentID()
+    {
+        return $this->comment_ID;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    public function getDate(): string
+    {
+        return $this->date;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTicket()
+    {
+        return $this->ticket;
+    }
+
 }
