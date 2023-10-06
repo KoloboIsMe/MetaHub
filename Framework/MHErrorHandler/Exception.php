e6439a82c403716c7a88074d8d5f3d908a0f649b
Gabriel <gabriel.rouge@etu.univ-amu.fr>
2023-10-06 11:16:39 +0200

    Catcher->exception
    Signed-off-by: Gabriel <gabriel.rouge@etu.univ-amu.fr>

--- Framework/MHErrorHandler/Exception.php
+++ Framework/MHErrorHandler/Exception.php
@@ -1,13 +1,16 @@
 <?php
 
-class Exception implements Throwable
+namespace MHErrorHandler;
+
+class Exception implements \MHErrorHandler\Throwable
     {
         private $message;
         private $level;
         private $time;
 
-        public function __construct($message ="Undefined Error", $level = 0)
+        public function __construct($message ="Undefined Exception", $level = 0)
         {
+            global $categoryDatabase;
             $this->message = $message;
             $this->level = $level;
             $this->time = date("Y-m-d H:i:s", time());
