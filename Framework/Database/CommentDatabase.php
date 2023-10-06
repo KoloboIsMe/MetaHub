09295e7adb5e505317d45df0a30602368915b6f8
Myke Lieto <myke.lieto@etu.univ-amu.fr>
2023-10-06 09:50:42 +0200

    Modification de la classe CommentDatabase, ajout de la fonction insert

--- Framework/Database/CommentDatabase.php
+++ Framework/Database/CommentDatabase.php
@@ -1,10 +1,30 @@
 <?php
 
 namespace Database;
+
 class CommentDatabase
 {
-    private $data;
-    public function getData(){
-        return $this->data;
+    private $PDO;
+
+    public function __construct()
+    {
+        $this->PDO = (new dataBaseConnexion())->getPDO();
     }
+
+    public function insert($comment)
+    {
+        $statement = $this->PDO->prepare(
+            "INSERT INTO user (comment_ID, text, date, author, ticket) VALUES (:comment_ID, :text, :date, :author, :ticket)");
+        if(!($statement->execute([
+            ':comment_ID' => $comment->getCommentID(),
+            ':text' => $comment->getText(),
+            ':date' => $comment->getDate(),
+            ':author' => $comment->getAuthor(),
+            ':ticket' => $comment->getTicket(),
+        ]))){
+            echo "erreur requete (exception)";
+            return null;
+        }
+    }
+
 }
