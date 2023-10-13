785000321cca86e3fd8a12f733da58cdc0717df5
Myke Lieto <myke.lieto@etu.univ-amu.fr>
2023-10-13 11:52:33 +0200

    Modif CommentsDataBase pas fini

--- Framework/Database/CommentDatabase.php
+++ Framework/Database/CommentDatabase.php
@@ -27,4 +27,29 @@ class CommentDatabase
         }
     }
 
+    public function removeComment($data){
+        $statement = $this->PDO->prepare("DELETE FROM COMMENT WHERE ID = :data;");
+        if(!($statement->execute([
+            'data' => $data
+        ]))){
+            echo "erreur requete (exception)";
+            return null;
+        }
+    }
+
+    public function selectAllTicket(){
+        $statement = $this->PDO->prepare("SELECT * FROM COMMENT;");
+        if(!($statement->execute([]))){
+            echo "erreur requete (exception)";
+            return null;
+        }
+        $comments = [];
+        $cpt = 0;
+        while ($ticket = $statement->fetch(PDO::FETCH_OBJ)) {
+            $post = new Comment($ticket->comment_ID, $ticket->title, $ticket->message, $ticket->date);
+            $comments[$cpt] = $post;
+            $cpt++;
+        }
+        return $comments;
+    }
 }
