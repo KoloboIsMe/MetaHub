65ea824ad4ba04f57d55cb81636b58fbc85931b5
Gabriel Rouge <gabriel.rouge@etu.univ-amu.fr>
2023-10-14 12:28:17 +0200

    Initialisation de l'Autoloader

--- Framework/Autoloader/Autoloader.php
+++ Framework/Autoloader/Autoloader.php
@@ -8,4 +8,26 @@ class Autoloader
     {
 
     }
+
+    public function load(string|Stringable $directory)
+    {
+        // IF directory is absolute, let it that way.
+        // ELSE, make it absolute
+        while (true) {
+            $file = ""; // Select a file
+            try
+            {
+                // Load the file
+            }
+            catch (\Exception $exception)
+            {
+                // Throw an error
+            }
+        }
+
+    }
+
+    public function singletons() {
+        // Instanciate all singletons
+    }
 }
