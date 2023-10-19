310c13994401f90bf83980855cfe46fe65506da1
Alexandre possenti <alex.possenti0@gmail.com>
2023-10-19 11:28:57 +0200

    ajout login et register non fonctionnels

--- app/Controllers/BlogController.php
+++ app/Controllers/BlogController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controllers;
 
-use Database\DBConnection;
 use Framework\managers\TicketManager;
 
 class BlogController extends Controller
@@ -12,12 +11,16 @@ class BlogController extends Controller
     }
 
     public function posts(){
+        if(!isset($_SESSION['user_ID']))
+            header('Location: /metahub/login');
         $ticketManager = new TicketManager();
         $tickets = $ticketManager->getTickets();
         return $this->view('blog.posts', compact('tickets'));
     }
 
     public function show(int $id){
+        if(!isset($_SESSION['user_ID']))
+            return $this->view('connexion.login');
         $ticketManager = new TicketManager();
         $ticket = $ticketManager->getTicketsWithCondition($id);
         return $this->view('blog.show', compact('id', 'ticket'));
