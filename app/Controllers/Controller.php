95bf0a5f6e2d7dffb675f39ba58206c360560aa0
Alexandre possenti <alex.possenti0@gmail.com>
2023-10-19 02:23:33 +0200

    ajout fichier

---
+++ app/Controllers/Controller.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controllers;
+
+use Database\DBConnection;
+
+class Controller
+{
+    public function view(string $path, array $params = null)
+    {
+        ob_start();
+        $path = str_replace('.', DIRECTORY_SEPARATOR, $path);
+        require VIEWS . $path . '.php';
+        if($params){
+            $params = extract($params);
+        }
+        $content = ob_get_clean();
+        require VIEWS . 'layout.php';
+    }
+}
