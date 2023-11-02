c1fa09df2442b99c57b8ad77d00e98341af27d72
Gabriel Rouge <gabriel.rouge@etu.univ-amu.fr>
2023-11-02 21:22:03 +0100

    Modif

--- Controler/Initialize/singletons.php
+++ Controler/Initialize/singletons.php
@@ -5,16 +5,15 @@
 /// Instantiates all Singletons.
 
 try {
-    $dbAdmin = \Framework\database\Connexion::getInstance("serveur_admin");
-    $dbLector = \Framework\database\Connexion::getInstance("serveur_lecture");
+    $connexion = \Framework\database\Connexion::getInstance("serveur_lecture");
 } catch (PDOException $e) {
     print "Erreur de connexion !: " . $e->getMessage() . "<br/>";
     die();
 }
 
-$categoryTable = new Framework\Database\Table\CategoryTable($dbLector);
-$commentTable = new Framework\Database\Table\CommentTable($dbLector);
-$ticketTable = new Framework\Database\Table\TicketTable($dbLector);
-$userTable = new Framework\Database\Table\UserTable($dbLector);
+$categoryTable = new Framework\Database\Table\CategoryTable($connexion);
+$commentTable = new Framework\Database\Table\CommentTable($connexion);
+$ticketTable = new Framework\Database\Table\TicketTable($connexion);
+$userTable = new Framework\Database\Table\UserTable($connexion);
 
 return;
