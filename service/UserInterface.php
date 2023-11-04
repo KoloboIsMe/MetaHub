b56955e5d55a581a54de95bbeae887042de970e7
Alexandre possenti <alex.possenti0@gmail.com>
2023-11-04 12:36:11 +0100

    ajout methode dans interfaces

--- service/UserInterface.php
+++ service/UserInterface.php
@@ -12,4 +12,8 @@ interface UserInterface
     public function isUser($login, $password);
 
     public function register($username, $password, $date);
+    public function updateLastConnexion($user_ID);
+    public function getUserById($id);
+    public function getUsersID();
+    public function getPostsIdByUserId($userId);
 }
