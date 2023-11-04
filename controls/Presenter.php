acaa6fb3715cce3e00b5b074670455020dc4e521
Alexandre possenti <alex.possenti0@gmail.com>
2023-11-05 00:18:32 +0100

    view avec scripts

--- controls/Presenter.php
+++ controls/Presenter.php
@@ -16,73 +16,11 @@ class Presenter
 
     public function showHomePage()
     {
-        $content = '';
-   $content .= "<h2>Fil d'actualité</h2>
-                <div class='card-container1'>";
-
-            for ($i = 0; $i < 5; $i++) {
-                if ($this->outputData->getOutputData()[$i] instanceof Post) {
-                    $post = $this->outputData->getOutputData()[$i];
-                    $id = $post->getTicket()->getTicket_ID();
-                    $content .= "
-                    <div class='card'>
-                        <a href='posts&id=$id'>
-                        <div class='card-content'>
-                            <div class='post-header'>
-                                <img src='gui/images/user.png' id='userImg'>
-                                <p id='card-username'>@" . $post->getUser()->getUsername() . "</p>
-                            </div>
-                            <h3> " . $post->getTicket()->getTitle() . "</h3>
-                            <p>" . $post->getTicket()->getMessage() . "</p>
-                            <time id='time'><B>Publié le " . $post->getTicket()->getDate() . "</B> </time>
-                            <p id='post-number'>Post n° " . $post->getTicket()->getTicket_ID() . "</p>";
-
-            if ((isset($_SESSION['level']) && $_SESSION['level'] > 0) || (isset($_SESSION['level']) && $_SESSION['user_ID'] == $post->getUser()->getUser_ID())) {
-                $content .= "<div class='edit-delete'>
-                                <a href='editTicket&id=$id'><img src='gui/images/edit.png' id='editImg'></a>
-                                <a href='/index.php?action=deleteTicketAction&id=$id'><img src='gui/images/delete.png' id='deleteImg'></a>
-                             </div>";
-            }
-          $content .= " </div>
-                        </a>
-                    </div>";
-                }
-            }
-        $content .= "
-                </div>
-                <div class='card-container2'>
-                    <div class='card2'>
-                        <div class='card-content'>
-                            <h3> Catégories</h3>
-                            <div class='category-list'>";
-                        for ($i = 0; $i < count($this->outputData->getOutputData()); $i++){
-                            if ($this->outputData->getOutputData()[$i] instanceof Category) {
-                                $category = $this->outputData->getOutputData()[$i];
-                                $id = $category->getCategory_ID();
-                                $content .= "<a href='categories&id=$id'><li><img src='gui/images/dot.png' class='dotImg'> #" . $category->getLabel() . "</li></a>";
-                            }
-                        }
-                    $content .="
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                           ";
-
-            $content.="
-                <div class='card-container2'>
-                    <div class='card3'>
-                        <div class='card-content'>
-                            <h3> Suggestions</h3>
-                            <p>Lorem ipsum dolor sit amet. In perferendis voluptas id quam omnis id explicabo sequi.
-                                Qui deserunt voluptatem ea fuga illum ut vero sunt et quis laudantium est temporibus enim.
-                                33 ducimus commodi eum voluptatem dolores est saepe nobis ea voluptatem molestias est
-                                natus eveniet non iste placeat qui commodi nobis.</p>
-                        </div>
-                    </div>
-                </div>";
-        return $content;
-
+        $data = $this->outputData->getOutputData();
+        extract($data);
+        ob_start();
+        require 'controls/pagePresenters/homepagePresenter.php';
+        return ob_get_clean();
     }
 
     public function showPosts()
---
+++ controls/pagePresenters/homepagePresenter.php
@@ -0,0 +1,60 @@
+<h2>Fil d'actualité</h2>
+<div class='card-container1'>
+    <?php
+
+    for ($i = 0; $i < 5; $i++) {
+        if ($data[$i] instanceof entities\Post) {
+            $post = $data[$i];
+            $id = $post->getTicket()->getTicket_ID();?>
+        <div class='card'>
+            <a href='posts&id=<?=$id?>'>
+                <div class='card-content'>
+                    <div class='post-header'>
+                        <img src='gui/images/user.png' id='userImg'>
+                        <p id='card-username'>@ <?= $post->getUser()->getUsername();?></p>
+                    </div>
+                    <h3> <?= $post->getTicket()->getTitle()?> </h3>
+                    <p>  <?= $post->getTicket()->getMessage()?></p>
+                    <time id='time'><B>Publié le   <?= $post->getTicket()->getDate()?></B> </time>
+                    <p id='post-number'>Post n°   <?= $post->getTicket()->getTicket_ID()?></p>
+
+        <?php if ((isset($_SESSION['level']) && $_SESSION['level'] > 0) || (isset($_SESSION['level']) && $_SESSION['user_ID'] == $post->getUser()->getUser_ID())) {?>
+                    <div class='edit-delete'>
+                        <a href='editTicket&id=$id'><img src='gui/images/edit.png' id='editImg'></a>
+                        <a href='/index.php?action=deleteTicketAction&id=$id'><img src='gui/images/delete.png' id='deleteImg'></a>
+                    </div>
+        <?php } ?>
+                </div>
+            </a>
+        </div>
+    <?php }
+    }?>
+</div>
+<div class='card-container2'>
+    <div class='card2'>
+        <div class='card-content'>
+            <h3> Catégories</h3>
+            <div class='category-list'>
+          <?php for ($i = 0; $i < count($data); $i++){
+                    if ($data[$i] instanceof entities\Category) {
+                    $category = $data[$i];
+                    $id = $category->getCategory_ID();?>
+                    <a href='categories&id=<?=$id?>'><li><img src='gui/images/dot.png' class='dotImg'> #<?= $category->getLabel()?> </li></a>
+              <?php }
+                } ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class='card-container2'>
+    <div class='card3'>
+        <div class='card-content'>
+            <h3> Suggestions</h3>
+            <p>Lorem ipsum dolor sit amet. In perferendis voluptas id quam omnis id explicabo sequi.
+                Qui deserunt voluptatem ea fuga illum ut vero sunt et quis laudantium est temporibus enim.
+                33 ducimus commodi eum voluptatem dolores est saepe nobis ea voluptatem molestias est
+                natus eveniet non iste placeat qui commodi nobis.</p>
+        </div>
+    </div>
+</div>
