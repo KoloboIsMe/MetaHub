8f47a8736e6e47e2cc78a73847b0deba60abc5dc
Gabriel Rouge <gabriel.rouge@etu.univ-amu.fr>
2023-11-05 11:02:33 +0100

    Correction CSS

--- View/Element/head.php
+++ View/Element/head.php
@@ -9,8 +9,8 @@ if (!isset($title))
 <head>
     <meta charset="UTF-8">
     <title><?php echo $title ?></title>
-    <link href="View/_assets/style/style.css" rel="stylesheet" type="text/css" />
-    <link href="View/_assets/style/login.css" rel="stylesheet" type="text/css" />
+    <link href="View/_assets/style/forms.css" rel="stylesheet" type="text/css" />
+    <link href="View/_assets/style/erreurs.css" rel="stylesheet" type="text/css" />
     <link href="View/_assets/style/CategorySelectionBar.css" rel="stylesheet" type="text/css" />
     <link href="View/_assets/style/layout.css" rel="stylesheet" type="text/css" />
     <link rel="shortcut icon" type="image/png" href="../_assets/image/MetaHubLogo.png"/>
